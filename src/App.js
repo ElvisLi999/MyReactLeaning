612fedc37f9a103de21d9f0343aaf07dfcbb8bcf
Elvis Li <presley.us@gmail.com>
2022-01-06 14:10:54 -0500

    using stateful lists and keys

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
-const App = () => {
-  const expenses = [
-    {id: 'e1', title: 'Toilet Paper', amount: 45.32, date: new Date(2020, 7, 14)},
-    {id: 'e2', title: 'New TV', amount: 799.80, date: new Date(2021, 2, 12)},
-    {id: 'e3', title: 'Car Insurance', amount: 587.33, date: new Date(2021, 2, 28)},
-    {id: 'e4', title: 'New Desk (Wooden)', amount: 359.99, date: new Date(2021, 5, 12)},
-  ];
+const DUMMY_EXPENSES = [
+  {id: 'e1', title: 'Toilet Paper', amount: 45.32, date: new Date(2020, 7, 14)},
+  {id: 'e2', title: 'New TV', amount: 799.80, date: new Date(2021, 2, 12)},
+  {id: 'e3', title: 'Car Insurance', amount: 587.33, date: new Date(2021, 2, 28)},
+  {id: 'e4', title: 'New Desk (Wooden)', amount: 359.99, date: new Date(2021, 5, 12)},
+];
 
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...expenses]
+    });
   };
 
   //{}:object that sets all the attributes
